0c8ecf2f473ab0d5a288f45c55694323f745d898
Nicholas Giordano <nicholas0g95@gmail.com>
2025-07-03 13:05:31 +0200

    feat: s3 backup destination

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ FROM mongo:latest
 # Get args
 ARG MONGO_URI
 
-# Install zip utility
-RUN apt-get update && apt-get install -y zip && rm -rf /var/lib/apt/lists/*
+# Install zip utility, s3cmd e dos2unix
+RUN apt-get update && apt-get install -y zip s3cmd dos2unix && rm -rf /var/lib/apt/lists/*
 
 # Set environment variables for MongoDB connection
 ENV MONGO_URI=$MONGO_URI
@@ -16,8 +16,8 @@ RUN mkdir -p /backup
 # Copy the backup script into the container
 COPY backup.sh /usr/local/bin/backup.sh
 
-# Make the script executable
-RUN chmod +x /usr/local/bin/backup.sh
+# Make the script executable e converti in formato Unix
+RUN dos2unix /usr/local/bin/backup.sh && chmod +x /usr/local/bin/backup.sh
 
 # Set the script as the container's entrypoint
 ENTRYPOINT ["/usr/local/bin/backup.sh"]
--- backup.sh
+++ backup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+export HOME=/root
+
+# Configura s3cmd con le variabili d'ambiente
+cat > /root/.s3cfg <<EOF
+[default]
+access_key = ${AWS_KEY}
+secret_key = ${AWS_SECRET}
+region = ${AWS_REGION}
+host_base = ${AWS_S3_ENDPOINT}
+host_bucket = ${AWS_S3_ENDPOINT}
+bucket_location = ${AWS_REGION}
+EOF
+
 # Assicurati che la variabile d'ambiente MONGO_URI sia impostata
 if [ -z "$MONGO_URI" ]; then
     echo "Errore: la variabile d'ambiente MONGO_URI non è impostata."
+    exit 1
+fi
+# Assicurati che la variabile d'ambiente S3_BUCKET sia impostata
+if [ -z "$AWS_S3_BUCKET" ]; then
+    echo "Errore: la variabile d'ambiente S3_BUCKET non è impostata."
     exit 1
 fi
 while true
@@ -14,6 +32,9 @@ do
     # Crea un archivio zip con la data e ora di oggi
     timestamp=$(date +"%Y%m%d_%H%M%S")
     zip -r "/backup/db_$timestamp.zip" /backup/dump
+
+    # Carica su S3 con s3cmd (forza il file di config)
+    s3cmd -c /root/.s3cfg put "/backup/db_$timestamp.zip" "s3://$AWS_S3_BUCKET/db_$timestamp.zip"
 
     # Rimuovi i file originali dopo averli archiviati
     rm -rf /backup/dump
